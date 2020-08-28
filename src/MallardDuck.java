5f29c978b53f8ac6ffeb74ed3cd5f6d5a45e1965
Adair Limana <adairjlimana@aluno.santoangelo.uri.br>
2020-08-28 00:57:30 -0300

    Commit inicial

---
+++ src/MallardDuck.java
@@ -0,0 +1,11 @@
+public class MallardDuck extends Duck {
+
+	public MallardDuck() {
+		this.quackBehavior = new Quack();
+		this.flyBehavior = new FlyWithWings();
+	}
+
+	public void display() {
+		System.out.println("I'm a real Mallard duck");
+	}
+}
